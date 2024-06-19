5c54bd5f0465e52ee08fe642af53710daaefedae
JosephOlu <161716305+JosephOluOlofinte@users.noreply.github.com>
2024-06-19 23:43:44 +0100

    update

---
+++ src/components/block/Footer.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Footer = () => {
+
+    return(
+
+        <FooterWrap>
+        </FooterWrap>
+    )
+}
+
+export default Footer;
+
+const FooterWrap = styled.section``
---
+++ src/components/block/Header.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Header = () => {
+
+    return(
+
+        <HeaderWrap>
+        </HeaderWrap>
+    )
+}
+
+export default Header;
+
+const HeaderWrap = styled.section``
